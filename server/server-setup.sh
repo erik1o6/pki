578a79637b0fb2fa703f73a4855c03598aba2e74
Erik Arfvidson <erik.arfvidson@blade-group.com>
2018-07-25 09:43:53 -0700

    cleanup

--- server/server-setup.sh
+++ server/server-setup.sh
@@ -32,16 +32,32 @@ cat << EOF > server-config.json
             "remote" : "intermediate",
             "auth_key" : "sand"
          }
-      },
-        "usages": [
-            "signing",
-            "key encipherment",
-            "server auth",
-            "client auth"
-        ]
+      }
    }
 }
-
+EOF
+cat << EOF > server-auth-config.json
+{
+    "auth_keys": {
+        "client": {
+            "type": "standard",
+            "key": "52abb3ac91971bb72bce17e7a289cd04476490b19e0d8eb7810dc42d4ac16c41"
+        }
+    },
+    "signing": {
+        "profiles": {
+            "client": {
+                "expiry": "8700h",
+                "auth_key": "client",
+                "usages": [
+                    "signing",
+                    "key encipherment",
+                    "client auth"
+                ]
+            }
+        }
+    }
+}
 EOF
 #cfssl gencert -ca=../intermediate-ca/intermediate-ca.pem -ca-key=../intermediate-ca/intermediate-ca-key.pem --config=../intermediate-ca/intermediate-ca-config.json -profile=server server-csr.json | cfssljson -bare server
-cfssl gencert -loglevel=0 -config server-config.json server-csr.json | cfssljson -bare db
+cfssl gencert -loglevel=0 -config server-config.json -profile server server-csr.json | cfssljson -bare server
